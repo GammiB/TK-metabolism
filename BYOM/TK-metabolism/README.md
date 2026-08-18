f501a39041d6c357cd6f1e6d0ba22998eb40e562
Johannes Raths <johannes.raths@eawag.ch>
2026-08-18 10:35:26 +0200

    Add files via upload

---
+++ BYOM/TK-metabolism/README.md
@@ -0,0 +1,11 @@
+This project repository is connected to the journal article ‘Pesticides Never Walk Alone: Validating Mixture Toxicity and Bioaccumulation Models in Soil Arthropods ’ by Wehrli et al. 2026 (doi TBA).
+
+The key element of this repository is the integration of a one-compartment TK model including phase-one biotransformation (TK-metabolism) and dynamic exposure in the BYOM platform (https://www.debtox.info/byom.html).
+
+How to run this model for your own work?  
+Navigate to "BYOM/TK-metabolism", run "pathdefine.m", use "byom\_metabolism\_template.m" as your template.
+
+
+
+After running the models, "extract\_data\_from\_fig\_template.m" can be used to export your model fits.
+
---
+++ BYOM/TK-metabolism/byom_metabolism_fludioxonil_reanalysis.m
@@ -0,0 +1,220 @@
+%% BYOM, byom_calanus_2016_onecomp.m 
+%
+% * Author: Tjalling Jager 
+% * Date: December 2021
+% * Web support: <http://www.debtox.info/byom.html>
+%
+% BYOM is a General framework for simulating model systems in terms of
+% ordinary differential equations (ODEs). The model itself needs to be
+% specified in <derivatives.html derivatives.m>, and <call_deri.html
+% call_deri.m> may need to be modified to the particular problem as well.
+% The files in the engine directory are needed for fitting and plotting.
+% Results are shown on screen but also saved to a log file (results.out).
+%
+% *The model:* An organism is exposed to a chemical in its surrounding
+% medium. The animal accumulates the chemical according to standard
+% one-compartment first-order kinetics.  
+%
+% *This script:* One-compartment TK of C2-naphthalene in _Calanus finmarchicus_.
+% 
+%  Copyright (c) 2012-2021, Tjalling Jager, all rights reserved.
+%  This source code is licensed under the MIT-style license found in the
+%  LICENSE.txt file in the root directory of BYOM. 
+%
+%
+% September 2022 
+% Modifications by AMD to include (irrevirsible) receptor binding of an 
+% antagonist. Here with the example of the antagonist thiacloprid (THI) 
+% binding to the nicotinic-acetylcholine receptor (nAChR) as observed 
+% in Gammarus pulex. in Raths et al. 2023
+
+% January 2026
+% Modifications by JR to include biotransformation. Receptor binding was
+% removed from this version of the script.
+
+
+%% Initial things
+% Make sure that this script is in a directory somewhere *below* the BYOM
+% folder.
+clear, clear global % clear the workspace and globals
+global DATA W X0mat % make the data set and initial states global variables
+global glo          % allow for global parameters in structure glo
+diary off           % turn of the diary function (if it is accidentaly on)
+set(0,'DefaultFigureWindowStyle','docked'); % collect all figure into one window with tab controls
+% set(0,'DefaultFigureWindowStyle','normal'); % separate figure windows
+
+pathdefine(0) % set path to the BYOM/engine directory (option 1 uses parallel toolbox)
+glo.basenm  = mfilename; % remember the filename for THIS file for the plots
+glo.saveplt = 1; % save all plots as (1) Matlab figures, (2) JPEG file or (3) PDF (see all_options.txt)
+
+%% The data set
+% Data are entered in matrix form, time in rows, scenarios (exposure
+% concentrations) in columns. First column are the exposure times, first
+% row are the concentrations or scenario numbers. The number in the top
+% left of the matrix indicates how to calculate the likelihood:
+%
+% * -1 for multinomial likelihood (for survival data)
+% * 0  for log-transform the data, then normal likelihood
+% * 0.5 for square-root transform the data, then normal likelihood
+% * 1  for no transformation of the data, then normal likelihood
+
+
+% Internal concentrations of parent compound in target organism in [µmol/kg] 
+% equals input data for state 1.
+DATA{1} = [0.50    1	1   1
+0	0	0	0
+0.025	5.241	7.043	5.957
+0.05	14.087	10.624	12.596
+0.1	14.287	10.801	13.900
+0.2	10.783	11.222	12.565
+0.4	12.128	9.304	11.938
+0.8	8.075	11.058	9.335
+1.4	7.509	5.529	4.832
+1.425	2.145	1.717	2.506
+1.45	2.036	3.260	2.541
+1.5	1.242	1.016	0.816
+1.6	0.104	0.139	0.385
+1.8	0.000	0.132	0.165
+2.2	0.203	0.387	0.082
+2.8	0.105	0.000	0.000
+
+];
+
+% Internal concentrations of the primary transformation product (phase one metabolite) in [µmol/kg] 
+% equals input data for state 2
+% !!! important: comment this vector for state 2 out if no % biotransformation is used (a vector of only zeros will cause an error)
+DATA{2} = [0.50    1	1   1
+0	0	0	0
+0.025	1.178	1.261	0.680
+0.05	1.715	1.011	2.188
+0.1	1.503	1.106	1.621
+0.2	0.450	1.194	0.649
+0.4	1.681	0.534	0.000
+0.8	3.230	0.952	1.039
+1.4	1.179	0.435	0.866
+1.425	0.257	0.495	0.229
+1.45	0.039	0.456	0.454
+1.5	0.259	0.037	0.029
+1.6	0.016	0.012	0.014
+1.8	0.289	0.295	0.367
+2.2	0.269	0.364	0.269
+2.8	0.375	0.387	0.327
+
+
+
+];
+
+% If needed, weights for individual measurements can be defined
+% For this, uncommend the following line and specify your weights
+
+% W{1} = 21 * ones(size(DATA{1})-1); % i.e. each point is a pooled sample of 21 animals
+
+% In this data set, exposure was time-varying and reported as a series of
+% concentrations over time. Here, the scenario is used as a linear forcing
+% series (which has an analytical solution, and is thus much faster than
+% the ODE version). Double time entries can be used, to account for replicate and sampling variancs.
+% Unit: [µmol/L]
+Cw1 = [0.5        1
+0	41.2
+0.2	37.8
+0.4	34.1
+0.8	33.4
+1.4	31.9
+1.4001	0
+2.8     0
+
+
+];
+
+make_scen(4,Cw1); % prepare as linear-forcing function interpolation (can use glo.use_ode = 0)  
+
+% Create a table with nicer labels for the legends
+Scenario = [1]; 
+Label = {'Model fit'};
+glo.LabelTable = table(Scenario,Label); % create a Matlab table for the labels
+
+%% Initial values for the state variables
+% Initial states, scenarios in columns, states in rows. First row are the
+% 'names' of all scenarios.
+
+X0mat(1,:) = Scenario; % scenarios (concentrations or identifiers)
+X0mat(2,:) = 0;      % initial values state 1 (parent internal concentrations)
+X0mat(3,:) = 0;      % initial values state 2 (1st BTP internal concentration)
+X0mat(4,:) = 0;      % initial values state 3 (total chemical internal concentration)
+
+%% Initial values for the model parameters
+% Model parameters are part of a 'structure' for easy reference. 
+
+% syntax: par.name = [startvalue fit(0/1) minval maxval scale];
+par.ke          = [5.724               1   0.001        50     2];  % elimination rate constant, d-1
+par.ku          = [9.465                1   0.1        80    2];  % uptake rate constant, L/kg/d
+
+par.km          = [26.81               1   1          100       2];  % 1stBTP biotransformation rate constant [d-1]
+par.kem         = [249.8               1    1           250     2];  % 1stBTP elimination rate constant [d-1]
+
+
+%% Time vector and labels for plots
+% Specify what to plot. If time vector glo.t is not specified, a default is
+% used, based on the data set
+
+% specify the y-axis labels for each state variable
+glo.ylab{1} = ['Concentration parent (umol/kg)'];
+glo.ylab{2} = ['Concentration metabolite (umol/kg)'];
+glo.ylab{3} = ['Total chemical concentration (umol/kg)'];
+
+% specify the x-axis label (same for all states)
+glo.xlab    = 'time (days)';
+glo.leglab1 = ''; % legend label before the 'scenario' number
+glo.leglab2 = [char(181),'M']; % legend label after the 'scenario' number
+
+prelim_checks % script to perform some preliminary checks and set things up
+% Note: prelim_checks also fills all the options (opt_...) with defauls, so
+% modify options after this call, if needed.
+% par_out = calc_optim(par,opt_optim); % start the optimisation
+% calc_and_plot(par_out,opt_plot); % call the plotting routine again to plot fits with CIs
+
+%% Calculations and plotting
+% Here, the function is called that will do the calculation and the plotting.
+% Options for the plotting can be set using opt_plot (see prelim_checks.m).
+% Options for the optimsation routine can be set using opt_optim. Options
+% for the ODE solver are part of the global glo. 
+
+opt_optim.type = 4; % optimisation method 1) simplex, 4) parameter-space explorer
+opt_optim.fit  = 1; % fit the parameters (1), or don't (0)
+opt_optim.it   = 0; % show iterations of the simplex optimisation (1, default) or not (0)
+opt_plot.bw    = 0; % plot in black and white
+opt_plot.cn    = 0; % if set to 1, connect model line to points (only for bw=1)
+opt_plot.annot = 1; % annotations in sub-plot: text box with parameter estimates or overall legend
+glo.useode     = 1; % use the analytical solution in simplefun.m (0) or the ODE solution in derivatives (1)
+glo.stiff      = 2; % 0 = use ode45 with very strict tolerances (default, non-stiff mode); 2 = use ode15s for experiments with long time (i.e. more than one week) or imbalanced paramters
+glo.R_mod = 1;  % default for print
+
+opt_optim.ps_plots = 0; % when set to 1, makes intermediate plots to monitor progress of parameter-space explorer
+opt_optim.ps_profs = 1; % when set to 1, makes profiles and additional sampling for parameter-space explorer
+opt_optim.ps_rough = 1; % set to 1 for rough settings of parameter-space explorer, 0 for settings as in openGUTS
+opt_optim.ps_saved = 0; % use saved set for parameter-space explorer (1) or not (0);
+
+% optimise and plot (fitted parameters in par_out)
+par_out = calc_optim(par,opt_optim); % start the optimisation
+% no plotting here; we'll immediately plot with CIs below
+
+%% Plot results with confidence intervals
+% The following code can be used to make a standard plot (the same as for
+% the fits), but with confidence intervals. Options for confidence bounds
+% on model curves can be set using opt_conf (see prelim_checks).
+% 
+% Use opt_conf.type to tell calc_conf which sample to use: 
+% -1) Skips CIs (zero does the same, and an empty opt_conf as well).
+% 1) Bayesian MCMC sample (default); CI on predictions are 95% ranges on 
+% the model curves from the sample 
+% 2) parameter sets from a joint likelihood region using the shooting 
+% method (limited sets can be used), which will yield (asymptotically) 95% 
+% CIs on predictions
+% 3) as option 2, but using the parameter-space explorer
+
+opt_conf.type    = 3; % make intervals from 1) slice sampler, 2) likelihood region shooting, 3) parspace explorer
+opt_conf.lim_set = 0; % use limited set of n_lim points (1) or outer hull (2, likelihood methods only) to create CIs
+opt_conf.sens    = 0; % type of analysis 0) no sensitivities 1) corr. with state, 2) corr. with state/control, 3) corr. with relative change of state over time
+
+out_conf = calc_conf(par_out,opt_conf);   % calculate confidence intervals on model curves
+calc_and_plot(par_out,opt_plot,out_conf); % call the plotting routine again to plot fits with CIs
